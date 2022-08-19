214ff542a10d0ed53234ff4409d184d5e60e2f42
adldtd <alexlytvynjr@gmail.com>
2022-08-19 17:17:22 -0400

    Made the save function use less memory

--- project/save.c
+++ project/save.c
@@ -6,45 +6,14 @@
 
 
 
-int save(unsigned char** colors, const char* seed, unsigned int lengthX, unsigned int lengthY)
+int save(unsigned char **colors, const char *seed, unsigned int lengthX, unsigned int lengthY)
 {
 	char file_path[100] = "";
 	char t[20] = ""; sprintf_s(t, 20, "%x", (unsigned int)time(NULL));
 	strcat(file_path, "map_"); strcat(file_path, t); strcat(file_path, ".png"); //Careful that buffer overflow does not occur here
 
 
-	png_byte** row_pointers = (png_byte**)malloc(sizeof(png_byte*) * lengthY); //Allocate memory for rows
-	if (!row_pointers)
-	{
-		printf("The program failed to allocate memory for the image.\n");
-		return 0;
-	}
-	
-	for (unsigned int i = 0; i < lengthY; i++)
-	{
-		row_pointers[i] = (png_byte*)malloc(sizeof(png_byte) * lengthX * 4);
-		if (!row_pointers[i])
-		{
-			printf("The program failed to allocate memory for row %i of the image.\n", i);
-			for (unsigned int r = 0; r < i; r++)
-				free(row_pointers[r]);
-			free(row_pointers);
-			return 0;
-		}
-
-		for (unsigned int j = 0; j < (lengthX * 4); j += 4) //************************************* WATCH OUT FOR POSSIBLE OVERFLOW HERE
-		{
-			row_pointers[i][j] = colors[i][j];
-			row_pointers[i][j + 1] = colors[i][j + 1];
-			row_pointers[i][j + 2] = colors[i][j + 2];
-			row_pointers[i][j + 3] = colors[i][j + 3];
-			//printf("%d %d %d %d\n", colors[i][j], colors[i][j + 1], colors[i][j + 2], colors[i][j + 3]);
-		}
-	}
-
-
-
-	FILE* img;
+	FILE *img;
 	fopen_s(&img, file_path, "wb");
 	if (!img)
 	{
@@ -78,7 +47,7 @@ int save(unsigned char** colors, const char* seed, unsigned int lengthX, unsigne
 
 	//png_set_invert_alpha(png_ptr);
 	png_write_info(png_ptr, png_info);
-	png_write_image(png_ptr, row_pointers);
+	png_write_image(png_ptr, colors);
 	
 	png_write_end(png_ptr, png_info);
 	png_destroy_write_struct(&png_ptr, &png_info);
@@ -89,9 +58,6 @@ int save(unsigned char** colors, const char* seed, unsigned int lengthX, unsigne
 	else
 		printf("Map saved as %s\\%s\n", cwd, file_path);
 
-	for (unsigned int r = 0; r < lengthY; r++)
-		free(row_pointers[r]);
-	free(row_pointers);
 	fclose(img);
 
 	return 1;
